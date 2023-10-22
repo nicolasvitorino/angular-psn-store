c55805dac97f9081a0e70165d3917bc6265820f0
nicolas <nicolasvitoorino@gmail.com>
2023-10-22 14:12:49 -0300

    creating psn store

---
+++ store/src/app/components/card/components/card/card-label/card-label.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-label',
+  templateUrl: './card-label.component.html',
+  styleUrls: ['./card-label.component.css']
+})
+export class CardLabelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ store/src/app/components/card/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
