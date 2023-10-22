9e61b403bbce583321174da86284749aef2ff6d2
nicolas <nicolasvitoorino@gmail.com>
2023-10-22 15:31:43 -0300

    psn store final version

--- store/src/app/components/card/card.component.ts
+++ store/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  @Input()
+  gameCover:string=""
+  @Input()
+  gameLabel:string=""
+  @Input()
+  gameType:string="Digital PS4"
+  @Input()
+  gamePrice:string="R$ 129,99"
+
 
   constructor() { }
 
