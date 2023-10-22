c0ec7584b732cb767022d14be6faee00a5c1b75d
nicolas <nicolasvitoorino@gmail.com>
2023-10-22 14:46:17 -0300

    creating card label and card pricing for the main card

--- store/src/app/components/card/components/card/card-label/card-label.component.ts
+++ store/src/app/components/card/components/card/card-label/card-label.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-label.component.css']
 })
 export class CardLabelComponent implements OnInit {
+
+  @Input()
+  gameLabel:string=""
 
   constructor() { }
 
--- store/src/app/components/card/components/card/card-pricing/card-pricing.component.ts
+++ store/src/app/components/card/components/card/card-pricing/card-pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-pricing.component.css']
 })
 export class CardPricingComponent implements OnInit {
+
+  @Input()
+  gameType:string="Digital PS4"
+  @Input()
+  gamePrice:string="R$ 129,99"
 
   constructor() { }
 
